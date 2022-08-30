0e3d67ab3bd1378828d51d7d66905e558fd4aabe
sunnatbee <sunnatbekyusupov@icloud.com>
2022-08-30 17:22:08 +0500

    ADd scrool

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var ElOpenBtn = document.querySelector(".site-header--js-open");
+var ElBodyScroll = document.querySelector("body")
 
 ElOpenBtn.addEventListener("click", function () {
   ElOpenBtn.closest(".site-header").classList.toggle("site-nav__open");
+  ElBodyScroll.classList.toggle("scroll");
 });
