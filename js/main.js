b8eb71cc56154e7480c2af43e89c618a99189537
sunnatbee <sunnatbekyusupov@icloud.com>
2023-02-10 03:53:49 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -5,3 +5,49 @@ ElOpenBtn.addEventListener("click", function () {
   ElOpenBtn.closest(".site-header").classList.toggle("site-nav__open");
   ElBodyScroll.classList.toggle("scroll");
 });
+
+
+$(".slider").slick({
+  centerMode: true,
+  centerPadding: "10px",
+  slidesToShow: 3,
+  variableWidth: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 1000,
+      settings: {
+        centerMode: true,
+        centerPadding: "0",
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        centerMode: true,
+        centerPadding: "0",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+
+// const video = document.getElementById("myVideo");
+
+// // Get the button
+// const btn = document.getElementById("myBtn");
+
+// // Pause and play the video, and change the button text
+// function myFunction() {
+//   if (video.paused) {
+//     video.play();
+//     btn.innerHTML = "Pause";
+//   } else {
+//     video.pause();
+//     btn.innerHTML = "Play";
+//   }
+// }
